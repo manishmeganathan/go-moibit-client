3f3e4e0237a1b056074e3f429724c958010e1c6d
Manish Meganathan <meganathan.manish@gmail.com>
2022-08-04 19:46:49 +0530

    Add FileStatus API
    - Renamed the FileStatus object to FileDescriptor
    - Added a method on Client, FileStatus that returns the status of the file at the given path.
    - Added a method Exists on FileDescriptor

--- filestat.go
+++ filestat.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 )
 
-// FileStatus describes the status of file
-type FileStatus struct {
+// FileDescriptor describes the status of file
+type FileDescriptor struct {
 	Active bool `json:"active"`
 	Enable bool `json:"enable"`
 
@@ -24,6 +24,12 @@ type FileStatus struct {
 	NodeAddress string `json:"nodeAddress"`
 }
 
+// Exists returns a boolean indicating if the
+// file exists based on if the hash is empty.
+func (file *FileDescriptor) Exists() bool {
+	return file.Hash == ""
+}
+
 // requestListFiles is the request for the ListFiles API of MOIBit
 type requestListFiles struct {
 	Path string `json:"path"`
@@ -32,13 +38,13 @@ type requestListFiles struct {
 // responseListFiles is the response for the ListFiles API of MOIBit
 type responseListFiles struct {
 	Metadata responseMetadata `json:"meta"`
-	Data     []FileStatus     `json:"data"`
+	Data     []FileDescriptor `json:"data"`
 }
 
 // ListFiles lists the files for a specified path.
-// The files are returned as a slice of FileStatus objects.
+// The files are returned as a slice of FileDescriptor objects.
 // An error is returned if the API fails or the client cannot authenticate with MOIBit.
-func (client *Client) ListFiles(path string) ([]FileStatus, error) {
+func (client *Client) ListFiles(path string) ([]FileDescriptor, error) {
 	// Generate Request Data
 	requestData, err := json.Marshal(requestListFiles{path})
 	if err != nil {
@@ -70,6 +76,58 @@ func (client *Client) ListFiles(path string) ([]FileStatus, error) {
 	decoder := json.NewDecoder(response.Body)
 	if err := decoder.Decode(resp); err != nil {
 		return nil, fmt.Errorf("response decode failed: %w", err)
+	}
+
+	// Returns the file descriptors from the response
+	return resp.Data, nil
+}
+
+// requestFileStatus is the request for the FileStatus API of MOIBit
+type requestFileStatus struct {
+	Path string `json:"path"`
+}
+
+// responseFileStatus is the response for the FileStatus API of MOIBit
+type responseFileStatus struct {
+	Metadata responseMetadata `json:"meta"`
+	Data     FileDescriptor   `json:"data"`
+}
+
+// FileStatus returns the status of a file at a specified path.
+// The returned FileStatus is empty if the file does not exist, which can be checked with Exists().
+// An error is returned if the API fails or the client cannot authenticate with MOIBit.
+func (client *Client) FileStatus(path string) (FileDescriptor, error) {
+	// Generate Request Data
+	requestData, err := json.Marshal(requestFileStatus{path})
+	if err != nil {
+		return FileDescriptor{}, fmt.Errorf("request serialization failed: %w", err)
+	}
+
+	// Generate Request Object
+	request, err := http.NewRequest("POST", urlListFiles, bytes.NewReader(requestData))
+	if err != nil {
+		return FileDescriptor{}, fmt.Errorf("request generation failed: %w", err)
+	}
+
+	// Set authentication headers from the client
+	client.setHeaders(request)
+
+	// Perform the HTTP Request
+	response, err := client.c.Do(request)
+	if err != nil {
+		return FileDescriptor{}, fmt.Errorf("request failed: %w", err)
+	}
+
+	// Check the status code of response
+	if response.StatusCode != 200 {
+		return FileDescriptor{}, fmt.Errorf("non-ok response: %v", response.StatusCode)
+	}
+
+	// Decode the response into a responseListFiles
+	resp := new(responseFileStatus)
+	decoder := json.NewDecoder(response.Body)
+	if err := decoder.Decode(resp); err != nil {
+		return FileDescriptor{}, fmt.Errorf("response decode failed: %w", err)
 	}
 
 	// Returns the file descriptors from the response
